650691e243904f51b1b76a25a2a31cbb8e12c254
leslie <alvarado.leslie12@yahoo.com>
2023-07-28 16:01:20 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+  return `This Saturday, I want to ${target}!`;
+}
+saturdayFun();
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")(
+  "a dedicated programmer"
+);
+console.log(encouragingPromptFunction);
